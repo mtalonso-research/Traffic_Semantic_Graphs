d941e5f3eb61732dada47eae10bf2f0feb9a1286
mtalonso-research <128753964+mtalonso-research@users.noreply.github.com>
2025-10-13 16:54:44 -0400

    Update main.py

--- runner_functions/main.py
+++ runner_functions/main.py
@@ -13,7 +13,6 @@ import sys
 from torch_geometric.utils import to_dense_batch
 import torch.optim as optim
 import json
-sys.path.append(os.path.abspath(".."))
 
 from functions.load_data_L2D import data_downloader
 from functions.process_tabular_data_L2D import process_tabular_data
@@ -21,6 +20,12 @@ from functions.process_tags_L2D import add_data_tags
 from functions.process_frames_L2D import process_frames
 from functions.process_lanes_L2D import lane_processing
 from functions.graphs import generate_graphs
+
+import argparse
+parser = argparse.ArgumentParser(description="Process L2D data.")
+parser.add_argument("--min_ep", type=int, default=0, help="Minimum episode number to process.")
+parser.add_argument("--max_ep", type=int, default=-1, help="Maximum episode number to process. Use -1 to process all available episodes.")
+args = parser.parse_args()
 
 def default_l2d_processing(min_ep,max_ep=-1):
     print('========== Downloading Data ==========')
@@ -34,15 +39,23 @@ def default_l2d_processing(min_ep,max_ep=-1):
                                     'observation.images.rear': False,
                                     'observation.images.right_backward': False,
                                     'observation.images.right_forward': False,
-                                }
-                            })
+                                }},
+                    tabular_data_dir = './data/raw/L2D/tabular',
+                    frames_dir = './data/raw/L2D/frames',
+                    metadata_dir = './data/raw/L2D/metadata',
+                    )
     print("========== Processing Tabular Data ==========")
     process_tabular_data(min_ep,max_ep,
                         overwrite=True, process_columns=True, 
                         process_osm=True, process_turning=True,
-                        time_sleep=2)
+                        time_sleep=2,
+                        source_dir = './data/raw/L2D/tabular',
+                        output_dir_processed = './data/processed_data/L2D',
+                        output_dir_tags = './data/semantic_tags/L2D')
     print("========== Adding Tags ==========")
-    add_data_tags(min_ep,max_ep)
+    add_data_tags(min_ep,max_ep,
+                    data_dir = './data/processed/L2D',
+                    tags_dir='./data/semantic_tags/L2D')
     print("========== Processing Frames ==========")
     process_frames(min_ep,max_ep,
                 cameras_on=["observation.images.front_left"],
@@ -50,7 +63,19 @@ def default_l2d_processing(min_ep,max_ep=-1):
                             "depth": True,
                             "speed": True,
                             'overwrite': True},
-                    verbose=False)
-    _ = lane_processing(min_ep,max_ep)
+                    verbose=False,
+                    input_base_dir = './data/raw/L2D/frames',
+                    output_base_dir = './data/processed_frames/L2D')
+    _ = lane_processing(min_ep,max_ep,
+                        input_base_dir = './data/raw/L2D/frames',
+                        output_base_dir = './data/processed_frames/L2D')
     print("========== Generate Graphs ==========")
-    generate_graphs(min_ep,max_ep)
+    generate_graphs(min_ep,max_ep,
+                    source_data_dir = './data/processed/L2D',
+                    processed_frame_dir = './data/processed_frames/L2D',
+                    output_dir = './data/graphical/L2D')
+
+if __name__ == "__main__":
+    min_episode = args.min_ep
+    max_episode = args.max_ep
+    default_l2d_processing(min_episode, max_episode)
